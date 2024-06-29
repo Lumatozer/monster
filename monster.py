9e04b13817a2dfea61ec6d4297eca23bef1712c3
aludayalu <pl46u3.ducklinz@gmail.com>
2024-06-29 19:29:59 +0530

    tokeniser fixed and parser added

--- monster.py
+++ monster.py
@@ -50,12 +50,6 @@ def render(path, variables=None):
         component=component.replace(x, out)
     return component
 
-def Tag():
-    return {"name":"", "attributes":{}, "signals":[], "children":[]}
-
-def Attribute():
-    return {"id":"", "value":""}
-
 def tokeniser(code):
     out=[]
     i=-1
@@ -79,7 +73,8 @@ def tokeniser(code):
             string_quote="\""
             continue
         if code[i]=="'":
-            out.append({"type":"quote", "value":"'"})
+            in_string=True
+            string_quote="\'"
             continue
         if code[i]=="<":
             if cache!="":
@@ -118,7 +113,7 @@ def tokeniser(code):
             continue
         if code[i]=="}":
             if cache!="":
-                out.append({"type":"variable", "value":"}"})
+                out.append({"type":"variable", "value":cache})
                 cache=""
             out.append({"type":"bracket", "value":"}"})
             continue
@@ -129,13 +124,63 @@ def tokeniser(code):
         out.append({"type":"variable", "value":cache})
     return out
 
-def parser(component):
+def parser(tokens, depth=0):
     out=[]
-    reading_tag=0
-    tag_name=""
-    for x in component:
-        if x=="<" and reading_tag==0:
-            reading_tag=1
+    i=-1
+    while True:
+        i+=1
+        if i>=len(tokens):
+            break
+        if tokens[i]["type"]=="operator" and tokens[i]["value"]=="<" and len(tokens)-i>=7:
+            j=i
+            tagStart=[]
+            while True:
+                j+=1
+                if j>=len(tokens):
+                    raise Exception("unexpected EOF while parsing HTML file")
+                if tokens[j]["type"]=="operator" and tokens[j]["value"]==">":
+                    break
+                tagStart.append(tokens[j])
+            if len(tagStart)==0 or tagStart[0]["type"]!="variable":
+                raise Exception("tag starting expected a token of type variable")
+            i=j
+            tagEnd=[]
+            count=1
+            while True:
+                j+=1
+                if j>=len(tokens):
+                    raise Exception("unexpected EOF while parsing HTML file"+str(depth))
+                tagEnd.append(tokens[j])
+                if len(tagEnd)>=4 and tagEnd[len(tagEnd)-4]["type"]=="operator" and tagEnd[len(tagEnd)-4]["value"]=="<" and tagEnd[len(tagEnd)-3]["type"]=="operator" and tagEnd[len(tagEnd)-3]["value"]=="/" and tagEnd[len(tagEnd)-2]==tagStart[0] and tagEnd[len(tagEnd)-1]["type"]=="operator" and tagEnd[len(tagEnd)-1]["value"]==">":
+                    count-=1
+                    if count==0:
+                        tagEnd=tagEnd[:len(tagEnd)-4]
+                        break
+                    continue
+                if len(tagEnd)>=2 and tagEnd[len(tagEnd)-2]["type"]=="operator" and tagEnd[len(tagEnd)-2]["value"]=="<" and tagEnd[len(tagEnd)-1]==tagStart[0]:
+                    count+=1
+                    continue
+            i=j
+            tagName=tagStart[0]["value"]
+            tagStart=tagStart[1:]
+            attributes={}
+            index=-1
+            while True:
+                index+=1
+                if index>=len(tagStart):
+                    break
+                if len(tagStart)-index>=3 and tagStart[index]["type"]=="variable" and tagStart[index+1]["type"]=="operator" and tagStart[index+1]["value"]=="=":
+                    if tagStart[index+2]["type"]!="string":
+                        attributes[tagStart[index]["value"]]={"type":"signal", "value":tagStart[index+2]["value"]}
+                    else:
+                        attributes[tagStart[index]["value"]]={"type":"raw", "value":tagStart[index+2]["value"]}
+                    index+=2
+                    continue
+                attributes[tagStart[index]["value"]]={"type":"raw", "value":"true"}
+            out.append({"type":"tag", "value":tagName, "children":parser(tagEnd, depth=depth+1), "attributes":attributes})
+            continue
+        out.append(tokens[i])
+    return out
 
 def init(app):
     MonsterApp=App()
