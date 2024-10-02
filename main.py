5e709edc7944ad85f79e4a1b75681e5016048cea
aludayalu <pl46u3.ducklinz@gmail.com>
2024-10-02 10:30:25 +0530

    added <py></py> tags server side rendering

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import request, redirect
-from monster import render, tokeniser, parser, Flask
+from monster import render, Flask
 import sys, json
 
 app = Flask(__name__)
@@ -7,8 +7,6 @@ app = Flask(__name__)
 @app.get("/")
 def home():
     signals=open("public/signals.js").read()
-    print(tokeniser(open("components/index.html").read()))
-    print(parser(tokeniser(open("components/index.html").read())))
     return render("index", locals())
 
 app.run(host="0.0.0.0", port=int(sys.argv[1]))
