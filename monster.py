5c44ab68621c02b1e0f7fc4bf19cf6ce86b34330
Aarav Dayal <pl46u3.ducklinz@gmail.com>
2024-10-08 21:45:01 +0530

    monster types changed to support amazon linux python 3.9

--- monster.py
+++ monster.py
@@ -40,7 +40,7 @@ class Render():
         self.render=render
 
 class Flask(Flask):
-    def __init__(self, import_name: str, static_url_path: str | None = None, static_folder: str | PathLike | None = "static", static_host: str | None = None, host_matching: bool = False, subdomain_matching: bool = False, template_folder: str | None = "templates", instance_path: str | None = None, instance_relative_config: bool = False, root_path: str | None = None):
+    def __init__(self, import_name: str, static_url_path: str = None, static_folder: str | PathLike = "static", static_host: str = None, host_matching: bool = False, subdomain_matching: bool = False, template_folder: str = "templates", instance_path: str = None, instance_relative_config: bool = False, root_path: str = None):
         super().__init__(import_name, static_url_path, static_folder, static_host, host_matching, subdomain_matching, template_folder, instance_path, instance_relative_config, root_path)
         @self.route('/<path:path>')
         def catch_all(path):
